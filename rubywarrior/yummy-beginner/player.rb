de81e442b0e155dd356751493dba00e646c48659
Marcin Kawczyński <zielony.mowi@gmail.com>
2018-07-26 16:17:25 +0200

    LEVEL_3

--- rubywarrior/yummy-beginner/player.rb
+++ rubywarrior/yummy-beginner/player.rb
@@ -1,9 +1,17 @@
 class Player
   def play_turn(warrior)
     if warrior.feel.empty?
-      warrior.walk!
+      if warrior.health>15
+        puts "HP" + warrior.health.to_s + " Idę dalej!"
+        warrior.walk!
+      else
+        warrior.rest!
+        puts "HP" + warrior.health.to_s + " Odpoczywam!"
+      end
     else
+      puts "Wróg atakuje. HP: " + warrior.health.to_s + " Oddam mu!!!"
       warrior.attack!
     end
+
   end
 end
